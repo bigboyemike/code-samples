32f0354c8b4a04aadb1d91b59ede9d5beb6ee084
Michael Frenkel <mfren00147@gmail.com>
2020-04-22 12:20:23 -0500

    help command change

--- help.py
+++ help.py
@@ -15,7 +15,7 @@ class help(commands.Cog):
             try:
                 await cmd.can_run(ctx)
                 #cmdlist.append(cmd.name)
-                cmdembed.add_field(name=f'**{cmd.name}**', value=(cmd.help if cmd.help else "None"))
+                cmdembed.add_field(name=f'**{cmd.signature}**', value=(cmd.help if cmd.help else "None"))
             except:
                 pass
         #embedcmds = discord.Embed(color=ctx.author.color, title=f'Available commands for {ctx.author.display_name}:', description='\n'.join(cmdlist), footer=f'Requested by {ctx.author}')
