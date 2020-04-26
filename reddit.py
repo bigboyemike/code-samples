a470fb3098af1b1bbca6f3fdddd2044c5b2d156d
Michael Frenkel <mfren00147@gmail.com>
2020-04-26 16:26:28 -0500

    post testing

--- reddit.py
+++ reddit.py
@@ -13,11 +13,46 @@ class reddit(commands.Cog):
                         username='TheHiMaster',
                         password='260426Mf')
 
+    postInfo = { "title":[],
+                "score":[],
+                "id":[], "url":[],
+                "comms_num": [],
+                "created": [],
+                "body":[]}
+    
     @commands.command()
     async def aaa(self, ctx):
+        
+        postInfo = { "title":[],
+                    "score":[],
+                    "id":[], "url":[],
+                    "comms_num": [],
+                    "created": [],
+                    "body":[],
+                    "author":[],
+                    "selftext":[]}
         subreddit = self.reddit.subreddit('dankmemes')
-        top_subreddit = subreddit.top(limit=2)
-        return await ctx.send(top_subreddit)
+        for submission in subreddit.top(limit=1):
+            postInfo["title"].append(submission.title)
+            postInfo["score"].append(submission.score)
+            postInfo["id"].append(submission.id)
+            postInfo["url"].append(submission.url)
+            postInfo["comms_num"].append(submission.num_comments)
+            postInfo["created"].append(submission.created)
+            postInfo["body"].append(submission.selftext)
+            postInfo["author"].append(submission.author)
+            postInfo["selftext"].append(submission.selftext)
+            if submission.is_self == True:
+                Text = True
+            else:
+                Text = False
+
+        postEmbed = discord.Embed(title=postInfo["title"], color=discord.Color.red(), description=postInfo["selftext"])
+        postEmbed.set_footer(text=f'Posted by {postInfo["author"]} with a score of {postInfo["score"]}. Post has {postInfo["comms_num"]} comments.')
+        if Text == False:
+            postEmbed.set_image(url=postInfo["url"])
+        return await ctx.send(embed=postEmbed)
+
 
 
 def setup(bot):
