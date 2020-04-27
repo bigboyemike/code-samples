3b157708795ced6755bf2415892245b0b5a599cc
Michael Frenkel <mfren00147@gmail.com>
2020-04-26 21:43:07 -0500

    a

--- reddit.py
+++ reddit.py
@@ -50,10 +50,12 @@ class reddit(commands.Cog):
             return await ctx.send(embed=postEmbed)
 
     @commands.command()
+    @commands.is_owner
     async def redditmsg(self, ctx, user, *, message):
         """Send a reddit message to a user"""
-        self.reddit.redditor(user).message(f'This is a message sent from {ctx.author} via Mikey#1211', message)
-        return ctx.message.add_reaction(emoji='<:check:688848512103743511>')
+        try:
+            self.reddit.redditor(user).message(f'This is a message sent from {ctx.author} via Mikey#1211', message)
+            return ctx.message.add_reaction(emoji='<:check:688848512103743511>')
         except:
             return await ctx.send('There was an error sending the message. Check to make sure you spelled the username correctly.')
 
