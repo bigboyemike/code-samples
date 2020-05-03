726eed35921b79209e1377034ca7038c81b33f87
Michael Frenkel <mfren00147@gmail.com>
2020-05-03 16:07:34 -0500

    music cog removal

--- music.py
+++ music.py
@@ -13,7 +13,7 @@ class music(commands.Cog):
     
     def __init__(self, bot):
         self.bot = bot
-        self.bot.music = lavalink.Client(self.bot.user.id)
+        #self.bot.music = lavalink.Client(self.bot.user.id)
         self.bot.music.add_node('localhost', 7000, 'testing', 'na', 'music-node')
         self.bot.add_listener(self.bot.music.voice_update_handler, 'on_socket_response')
         self.bot.music.add_event_hook(self.track_hook)
